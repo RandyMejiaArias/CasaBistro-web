5682662e2f7a34cf5a76354a8344a78d9415573c
Randy Mejía Arias <rdmejia3@espe.edu.ec>
2021-08-30 18:53:01 -0500

    Function to change nav color added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// Navegacion Pegada 
+$(document).ready(function () {
+  $(window).bind('scroll', function () {
+      var navHeight = $(window).height() * 0.02;
+      if ($(window).scrollTop() > navHeight) {
+          $('#nav').addClass('scrolled');
+          $('.button').addClass('scrolled');
+      } else {
+          $('#nav').removeClass('scrolled');
+          $('.button').removeClass('scrolled');
+      }
+  });
+});
+
 window.addEventListener('load', function(){
     new Glider(this.document.querySelector('.carousel-list'),{
         slidesToShow: 1,
@@ -33,3 +47,18 @@ window.addEventListener('load', function(){
           ]
     });
 });
+
+// var nav = $('#nav');
+
+// $(window).scroll(function(){
+//   if($(window).scrollTop() > 150){
+//     nav.addClass('scrolled');
+//   } else{
+//     nav.removeClass('scrolled');
+//   }
+// });
+
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:0}, '300');
+// });
