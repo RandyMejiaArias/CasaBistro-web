6a8311fbad52cd0e20b7cb88a7b8e3fd5fb1ca21
Randy Mejía Arias <rdmejia3@espe.edu.ec>
2021-09-09 18:43:15 -0500

    Carousel added to modals if is necessary

--- js/main.js
+++ js/main.js
@@ -48,6 +48,66 @@ window.addEventListener('load', function(){
     });
 });
 
+window.addEventListener('load', function(){
+  new Glider(this.document.querySelector('.modal-carousel-list-wingsx6'),{
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: false,
+      arrows: {
+        prev: '#prev-wingsx6',
+        next: '#next-wingsx6'
+      },
+  });
+});
+
+window.addEventListener('load', function(){
+  new Glider(this.document.querySelector('.modal-carousel-list-wingsx10'),{
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: false,
+      arrows: {
+        prev: '#prev-wingsx10',
+        next: '#next-wingsx10'
+      },
+  });
+});
+
+window.addEventListener('load', function(){
+  new Glider(this.document.querySelector('.modal-carousel-list-wingsx12'),{
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: false,
+      arrows: {
+        prev: '#prev-wingsx12',
+        next: '#next-wingsx12'
+      },
+  });
+});
+
+window.addEventListener('load', function(){
+  new Glider(this.document.querySelector('.modal-carousel-list-wingsx24'),{
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: false,
+      arrows: {
+        prev: '#prev-wingsx24',
+        next: '#next-wingsx24'
+      },
+  });
+});
+
+window.addEventListener('load', function(){
+  new Glider(this.document.querySelector('.modal-carousel-list-canoe-strip'),{
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: false,
+      arrows: {
+        prev: '#prev-canoe-strip',
+        next: '#next-canoe-strip'
+      },
+  });
+});
+
 // var nav = $('#nav');
 
 // $(window).scroll(function(){
