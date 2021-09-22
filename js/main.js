9c06c51e555a2e495358452c747ea445cf2bab94
Randy Mejía Arias <rdmejia3@espe.edu.ec>
2021-09-22 16:31:54 -0500

    Additional styles removed

--- js/main.js
+++ js/main.js
@@ -1,54 +1,4 @@
-// Navegacion Pegada 
-$(document).ready(function () {
-  $(window).bind('scroll', function () {
-      var navHeight = $(window).height() * 0.02;
-      if ($(window).scrollTop() > navHeight) {
-          $('#nav').addClass('scrolled');
-          // $('.button').addClass('scrolled');
-      } else {
-          $('#nav').removeClass('scrolled');
-          // $('.button').removeClass('scrolled');
-      }
-  });
-});
-
 $('.extras-menu-item-header span:first-child').after("<span class=\"dots\"> </span>");
-
-window.addEventListener('load', function(){
-    new Glider(this.document.querySelector('.carousel-list'),{
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: false,
-        arrows: {
-          prev: '.carousel-previous',
-          next: '.carousel-next'
-        },
-        responsive: [
-            {
-              // screens greater than >= 768px
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              }
-            },{
-                // screens greater than > 1440px
-                breakpoint: 1441,
-                settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 1,
-                }
-            }
-          ]
-    });
-});
 
 window.addEventListener('load', function(){
   new Glider(this.document.querySelector('.modal-carousel-list-wingsx6'),{
@@ -109,18 +59,3 @@ window.addEventListener('load', function(){
       },
   });
 });
-
-// var nav = $('#nav');
-
-// $(window).scroll(function(){
-//   if($(window).scrollTop() > 150){
-//     nav.addClass('scrolled');
-//   } else{
-//     nav.removeClass('scrolled');
-//   }
-// });
-
-// btn.on('click', function(e) {
-//   e.preventDefault();
-//   $('html, body').animate({scrollTop:0}, '300');
-// });
